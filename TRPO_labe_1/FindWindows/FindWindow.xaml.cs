5682bf9117442d5d03376bfd58029316506a8044
Mikhail Ilyushin <mishamine26@mail.ru>
2020-09-08 19:41:33 +0300

    Initial commit

---
+++ TRPO_labe_1/FindWindows/FindWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TRPO_labe_1.FindWindows
+{
+    /// <summary>
+    /// Логика взаимодействия для FindWindow.xaml
+    /// </summary>
+    public partial class FindWindow : Window
+    {
+        private MainWindow _mw;
+        public FindWindow(MainWindow MW)
+        {
+            InitializeComponent();
+            _mw = MW;
+        }
+
+        private void btnFind_Click(object sender, RoutedEventArgs e)
+        {
+            Regex reg = new Regex($"{findTextBox.Text}");
+            _mw.SelectText(reg.Matches(new TextRange(_mw.textBox.Document.ContentStart, _mw.textBox.Document.ContentEnd).Text));
+        }
+    }
+}
---
+++ TRPO_labe_1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TRPO_labe_1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFind_Click(object sender, RoutedEventArgs e)
+        {
+            var wind = new FindWindows.FindWindow(this);
+            wind.Show();
+        }
+
+        public void SelectText(MatchCollection regexCollection)
+        {
+            TextPointer t1, t2;
+            foreach (Match reg in regexCollection)
+            {
+                t1 = textBox.Document.ContentStart.GetPositionAtOffset(reg.Index);
+                t2 = textBox.Document.ContentStart.GetPositionAtOffset(reg.Length);
+                textBox.Selection.Select(t1, t2);
+                textBox.SelectionTextBrush = ;
+            } 
+        }
+    }
+}
